943fc7e977caf8cf37b7f17f142051e80ef45d18
ftngrn <sf.n.ishihara+github@gmail.com>
2013-01-17 16:44:30 +0900

    Add generate hash method for beforeSave #2 @0.5

--- app/Model/Mail.php
+++ app/Model/Mail.php
@@ -19,36 +19,6 @@ class Mail extends AppModel {
  * @var array
  */
 	public $validate = array(
-		'hash' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'from_user_id' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'to_user_id' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'uid' => array(
 			'numeric' => array(
 				'rule' => array('numeric'),
@@ -70,4 +40,19 @@ class Mail extends AppModel {
 			),
 		),
 	);
+
+	public function beforeSave($options = array()) {
+		$result = parent::beforeSave($options);
+		if (!$this->id && !isset($this->data[$this->alias]['id'])) {
+			//新規作成時にhashを生成する
+			$this->data[$this->alias]['hash'] = $this->generateHash();
+		}
+		return $result;
+	}
+
+	public function generateHash($byte_length = 8) {
+		$bytes = openssl_random_pseudo_bytes($byte_length);
+		$hash = bin2hex($bytes);
+		return $hash;
+	}
 }
